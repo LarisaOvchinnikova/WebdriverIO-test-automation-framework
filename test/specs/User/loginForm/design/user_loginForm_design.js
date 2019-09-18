c5cfebadb6ffd11436a39a1427efaee19af77b9c
Larisa <larisaplantation@gmail.com>
2019-09-17 23:56:21 -0400

    created new tests

--- test/specs/User/loginForm/design/user_loginForm_design.js
+++ test/specs/User/loginForm/design/user_loginForm_design.js
@@ -8,6 +8,12 @@ describe('User -- Login Form -- Design', () => {
         browser.url(url);
     });
 
+    it('should validate h1 is displayed', () => {
+        const selector = $('//h1');
+        const isDisplayed = selector.isDisplayed();
+        expect(isDisplayed).to.be.true;
+    });
+
     it('should have correct header', () => {
         const selector = $('//h1');
         const actual = selector.getText();
@@ -15,11 +21,25 @@ describe('User -- Login Form -- Design', () => {
         expect(actual).to.equal(expected);
     });
 
-    it('should validate h1 is displayed', () => {
+    it('should validate h1 has correct color', () => {
         const selector = $('//h1');
-        const isDisplayed = selector.isDisplayed();
-        expect(isDisplayed).to.be.true;
+        const actualColor = selector.getCSSProperty('color').parsed.hex;
+        const expectColor = '#333333';
+        expect(actualColor).to.equal(expectColor);
     });
 
+    it('should validate h1 has correct alignment', () => {
+        const selector = $('//h1');
+        const actualAlign = selector.getCSSProperty('text-align').parsed.string;
+        const expectAlign = 'left';
+        expect(actualAlign).to.equal(expectAlign);
+    });
+
+    it('should validate h1 has correct font-size', () => {
+        const selector = $('//h1');
+        const actualSize = selector.getCSSProperty('font-size').parsed.string;
+        const expectFontSize = '39.8539px';
+        expect(actualSize).to.equal(expectFontSize);
+    });
 
 });
