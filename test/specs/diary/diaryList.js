90fce0c05f55d35ba1db1a4fc6e9f5c90ee61007
Larisa <larisaplantation@gmail.com>
2019-09-18 18:10:36 -0400

    created folder diary and 2 tests

--- test/specs/diary/diaryList.js
+++ test/specs/diary/diaryList.js
@@ -1,24 +1,26 @@
-import { expect }  from 'chai';
-
-//import loginHelpers from './../../actions/loginAction';
-import loginAction from "../../actions/loginAction";
-
-//const { baseUrl } = require('./constants');
-
-//const url = `${baseUrl}/user/login`;
+//import { expect } from 'chai';
+const { expect } = require ('chai');
+import loginHelpers from '../../actions/loginAction';
 
 describe('Diary List', () => {
     before (() => {
         loginHelpers.login();
     });
-/*
-    it('should have correct ', () => {
-        const selector = '';
-        const element = $(selector);
-        const actual = element.getText();
-        const expected = '';
 
-        expect(actual).to.equal(expected);
+    it('should have main menu item ', () => {
+      //  const selector = '//div[@id="site-menu"]//a[text() ="Diary"]';
+        const selector = '//a[contains(text(),\'Diary\')]';
+        const isDisplayed = $(selector).isDisplayed();
+        expect(isDisplayed).to.be.true;
     });
-*/
+
+    it('should redirect to Diary page ', () => {
+        const selector = '//a[contains(text(),\'Diary\')]';
+        $(selector).click();
+
+        const actualh1Text = $('//h1').getText();
+        const expected = 'Day reports';
+
+        expect(actualh1Text).to.equal(expected);
+    });
 });
