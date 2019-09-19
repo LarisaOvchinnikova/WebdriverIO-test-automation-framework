b065ddffcbc6ec53e63f630063f5f5dd2e09079e
Larisa <larisaplantation@gmail.com>
2019-09-19 19:48:22 -0400

    added tests

--- test/specs/homePage/general.js
+++ test/specs/homePage/general.js
@@ -4,8 +4,7 @@ const assert = require('chai').assert;
 
 const { baseUrl } = require('./../../constants');
 const url = `${baseUrl}`;
-console.log('--------------------------------------------------------------------------------')
-console.log(url);
+
 describe('Home page - General', () => {
     before (() => {
         browser.url(url);
@@ -19,10 +18,11 @@ describe('Home page - General', () => {
 
     it('should veryfy that favicon.ico is correct', function () {
         const selector = '//link[@href="/favicon.ico"]';
-        const faviconIsDisplayed = $(selector).isDisplayed();
+        const faviconIsDisplayed = $(selector).isExisting();
         expect(faviconIsDisplayed).to.be.true;
 
     });
+
     it('should have correct title', () => {
         const title = browser.getTitle();
         browser.pause(4000);
@@ -30,7 +30,7 @@ describe('Home page - General', () => {
         expect(title).to.equal(expected);
     });
 
-    it('verify that main bar contains site name', () => {
+    it('verify that application name is displayed', () => {
         const selector = $('//span[@id="site-name"]');
         const actual = selector.getText();
         const expected = 'Progress Monitor';
@@ -38,54 +38,21 @@ describe('Home page - General', () => {
         expect(actual).to.equal(expected);
     });
 
-    it('application name should have correct alignment', () => {
+    it('application has global header', () => {
         const selector = $('//span[@id="site-name"]');
         const actualAlign = selector.getCSSProperty('text-align').parsed.string;
         const expectAlign = 'left';
         expect(actualAlign).to.equal(expectAlign);
     });
 
-    it('application name should have correct color', () => {
-        const selector = $('//span[@id="site-name"]');
-        const actualColor = selector.getCSSProperty('color').parsed.hex;
-        const expectColor = '#000000';
-        expect(actualColor).to.equal(expectColor);
+    it('application has global footer', function () {
+
     });
 
-    it('application name should have correct font-size', () => {
-        const selector = $('//span[@id="site-name"]');
-        const actualSize = selector.getCSSProperty('font-size').parsed.string;
-        const expectFontSize = '20px';
-        expect(actualSize).to.equal(expectFontSize);
-    });
+    it('application has container with functional parts 8 images and  8 blocks', function () {
 
-    it('application name should have correct font-weight', () => {
-        const selector = $('//span[@id="site-name"]');
-        const actualWeight = selector.getCSSProperty('font-weight').parsed.string;
-        const expectFontWeight = '500';
-        expect(actualWeight).to.equal(expectFontWeight);
-    });
 
-    it('application name should have correct font-family ', function () {
-        const elem = $('//span[@id="site-name"]');
-        const font = elem.getCSSProperty('font-family').parsed.string ;
-        expect(font).to.equal('"sf pro display", "sf pro icons", "helvetica neue", helvetica, arial, sans-serif');
     });
-
-    it('should have Login button', () => {
-        const login = $('//a[@class="nav-link"]');
-        const actual = login.getText();
-        const expected = 'Login';
-        expect(actual).to.equal(expected);
-    });
-
-    it('should have Register button', () => {
-        const reg = $('//a[@class=\'btn btn-outline-primary\']');
-        const actual = reg.getText();
-        const expected = 'Register';
-        expect(actual).to.equal(expected);
-    });
-
     it('should have correct year in the footer copywrite line', () => {
         const element = $('//small[@class=\"d-block mb-3 text-muted\"]');
         const text = element.getText();
