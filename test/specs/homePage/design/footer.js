d647d2867ba576f661894c8bac1a83308e6c53e5
Larisa <larisaplantation@gmail.com>
2019-09-22 19:10:13 -0400

    added file footer (home page - design)

---
+++ test/specs/homePage/design/footer.js
@@ -0,0 +1,78 @@
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+//import {expect} from 'chai';
+
+describe('Home page - footer - design', () => {
+    before (() => {
+        browser.url('https://stage.pasv.us/');
+    });
+
+    it('footer is displayed', function () {
+        const selector = '//footer/div[@class="container"]';
+        const footerIsDisplayed = $(selector).isDisplayed();
+        expect(footerIsDisplayed).to.be.true;
+    });
+
+    it('should verify that first line contains Version', function () {
+        const footer = @('//div[contains(text(),"Version")]');
+
+    });
+
+    it('should display current version ', function () {
+        const version = @('//span[@class=\'badge badge-light\']');
+        const expected ='0.1.74';
+        expect(version).equal(expected);
+    });
+    it('should have correct year in the footer copywrite line', () => {
+        const element = $('//small[@class=\"d-block mb-3 text-muted\"]');
+        const text = element.getText();
+        const currentYear = '2019';
+        const isIncludes = text.includes(currentYear);
+        expect(isIncludes).to.be.true;
+    });
+
+    it('should have correct text in the footer copywrite line', () => {
+        const element = $('//small[@class=\"d-block mb-3 text-muted\"]');
+        const actual = element.getText();
+        const currentText = '© 2019 eat(); sleep(); code(); repeat();';
+        expect(actual).to.include(currentText);
+    });
+
+    it('application name should have correct alignment', () => {
+        const selector = $('//span[@id="site-name"]');
+        const actualAlign = selector.getCSSProperty('text-align').parsed.string;
+        const expectAlign = 'left';
+        expect(actualAlign).to.equal(expectAlign);
+    });
+
+    it('application name should have correct color', () => {
+        const selector = $('//span[@id="site-name"]');
+        const actualColor = selector.getCSSProperty('color').parsed.hex;
+        const expectColor = '#000000';
+        expect(actualColor).to.equal(expectColor);
+    });
+
+    it('application name should have correct font-size', () => {
+        const selector = $('//span[@id="site-name"]');
+        const actualSize = selector.getCSSProperty('font-size').parsed.string;
+        const expectFontSize = '20px';
+        expect(actualSize).to.equal(expectFontSize);
+    });
+
+    it('application name should have correct font-weight', () => {
+        const selector = $('//span[@id="site-name"]');
+        const actualWeight = selector.getCSSProperty('font-weight').parsed.string;
+        const expectFontWeight = '500';
+        expect(actualWeight).to.equal(expectFontWeight);
+    });
+
+    it('application name should have correct font-family ', function () {
+        const elem = $('//span[@id="site-name"]');
+        const font = elem.getCSSProperty('font-family').parsed.string ;
+        expect(font).to.equal('"sf pro display", "sf pro icons", "helvetica neue", helvetica, arial, sans-serif');
+    });
+
+
+
+
+});
