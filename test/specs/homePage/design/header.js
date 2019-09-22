63a3c315c44018e9252acdc951168c017a996902
Larisa <larisaplantation@gmail.com>
2019-09-21 21:28:07 -0400

    added test on design header (Home page)

--- test/specs/homePage/design/header.js
+++ test/specs/homePage/design/header.js
@@ -1,28 +1,27 @@
 const expect = require('chai').expect;
 const assert = require('chai').assert;
-//import {expect} from 'chai';
 
 const { baseUrl } = require('../../../constants');
 const url = `${baseUrl}`;
 
-describe('Home page - General', () => {
-    before (() => {
+describe('Home page - Design', () => {
+    before(() => {
         browser.url(url);
     });
 
-    it('should verify URL', function () {
-        const applicationURL = browser.getUrl();
-        const expectedURL = 'https://stage.pasv.us/';
-        expect(applicationURL).to.equal(expectedURL);
-    });
-
-    it('should veryfy that favicon.ico is existing', function () {
-        const selector = '//link[@href="/favicon.ico"]';
-        const faviconIsDisplayed = $(selector).isExisting();
-        expect(faviconIsDisplayed).to.be.true;
+    it('should veryfy that application name is displayed', function () {
+        const siteName = '//span[@id="site-name"]';
+        const siteNameIsDisplayed = $(selector).isDisplayed();
+        expect(siteNameIsDisplayed).to.be.true;
 
     });
 
+    it('verify that application name is "Progress <onitor"', () => {
+        const selector = '//span[@id="site-name"]';
+        const siteName = $(selector).getText();
+        const expectedSiteName = 'Progress Monitor';
+        expect(siteName).to.equal(expectedSiteName);
+    });
     it('should have correct title', () => {
         const title = browser.getTitle();
         browser.pause(4000);
@@ -36,13 +35,7 @@ describe('Home page - General', () => {
         expect(globalHeader).to.be.true;
     });
 
-    it('verify that header contains application name', () => {
-        const selector = '//span[@id="site-name"]';
-        const actual = $(selector).getText();
-        const expected = 'Progress Monitor';
 
-        expect(actual).to.equal(expected);
-    });
 
     it('should verify that h1 header is correct', function () {
         const selector = '//div[@class ="header-title"]'
@@ -54,3 +47,4 @@ describe('Home page - General', () => {
         expect(h1Actual).equal(h1Expected);
     });
 
+});
