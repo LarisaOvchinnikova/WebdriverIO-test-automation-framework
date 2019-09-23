a050390243aa349f52dae7772d720dfc8a46a01b
Larisa <larisaplantation@gmail.com>
2019-09-22 20:26:19 -0400

    added test to header(home page - design)

--- test/specs/homePage/design/header.js
+++ test/specs/homePage/design/header.js
@@ -89,13 +89,6 @@ describe('Home page - Design', () => {
         expect(actual).to.equal(expected);
     });
 
-    it('verify that navigation bar has Register button', () => {
-        const registerButton = $('//a[@class="btn btn-outline-primary"]');
-        const actual = registerButton.getText();
-        const expected = 'Register';
-        expect(actual).to.equal(expected);
-    });
-
     it('verify that Login button has correct text-color', () => {
         const selector = $('//a[@class="nav-link"]');
         const actualColor = selector.getCSSProperty('color').parsed.hex;
@@ -133,6 +126,56 @@ describe('Home page - Design', () => {
 
     it('verify that Login button has correct font-family', () => {
         const selector = '//a[@class="nav-link"]';
+        const elem = $(selector);
+        const font = elem.getCSSProperty('font-family').parsed.string ;
+        expect(font).to.equal('"sf pro display", "sf pro icons", "helvetica neue", helvetica, arial, sans-serif');
+    });
+
+
+    it('verify that navigation bar has Register button', () => {
+        const registerButton = $('//a[@class="btn btn-outline-primary"]');
+        const actual = registerButton.getText();
+        const expected = 'Register';
+        expect(actual).to.equal(expected);
+    });
+
+    it('verify that Register button has correct text-color', () => {
+        const selector = $('//a[@class="btn btn-outline-primary"]');
+        const actualColor = selector.getCSSProperty('color').parsed.hex.toLowerCase();
+        const expectColor = '#0052cc';
+        expect(actualColor).to.equal(expectColor);
+    });
+
+    it('verify that Login button has correct background-color', () => {
+        const selector = $('//a[@class="btn btn-outline-primary"]');
+        const actualColor = selector.getCSSProperty('background-color').parsed.hex;
+        const expectColor = '#000000';
+        expect(actualColor).to.equal(expectColor);
+    });
+
+    it('verify that Login button has font-size 17px', () => {
+        const selector = $('//a[@class="btn btn-outline-primary"]');
+        const actualSize = selector.getCSSProperty('font-size').parsed.string;
+        const expectFontSize = '17px';
+        expect(actualSize).to.equal(expectFontSize);
+    });
+
+    it('verify that Login button has font-weight 400', () => {
+        const selector = $('//a[@class="btn btn-outline-primary"]');
+        const actualWeight = selector.getCSSProperty('font-weight').parsed.string;
+        const expectFontWeight = '400';
+        expect(actualWeight).to.equal(expectFontWeight);
+    });
+
+    it('verify that text-align of Login button is left', () => {
+        const selector = $('//a[@class="btn btn-outline-primary"]');
+        const actualAlignment = selector.getCSSProperty('text-align').parsed.string;
+        const expectAlignment = 'left';
+        expect(actualAlignment).to.equal(expectAlignment);
+    });
+
+    it('verify that Login button has correct font-family', () => {
+        const selector = '//a[@class="btn btn-outline-primary"]';
         const elem = $(selector);
         const font = elem.getCSSProperty('font-family').parsed.string ;
         expect(font).to.equal('"sf pro display", "sf pro icons", "helvetica neue", helvetica, arial, sans-serif');
