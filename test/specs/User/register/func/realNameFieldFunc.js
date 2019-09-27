5baddddfe18719d992fd4cd90496ba4a1a4ab187
Larisa <larisaplantation@gmail.com>
2019-09-27 17:15:24 -0400

    added func test for Real name field

---
+++ test/specs/User/register/func/realNameFieldFunc.js
@@ -0,0 +1,22 @@
+import { expect }  from 'chai';
+import { baseUrl } from './../../../../constants';
+
+const url = `${baseUrl}/user/register`;
+
+const positiveNames = ["John Johnson", "Johnjohnjohnjoh Johnjohnjohnjoh", "Jo Jo"];
+const negativeNames = ["John1 Johnson1", "Johnjohnjohnjohnnn Johnjohnjohnjohnnn", "J J"];
+describe("Verify the functionality of Real Name field on Registion page", () => {
+    before(() => {
+        browser.url(url);
+        browser.maximizeWindow();
+    });
+        positiveNames.forEach(name => {
+            it(`Verify that '${name}' is be valid`, () => {
+                const realNameField = $('//input[@name="name"]');
+                realNameField.setValue(name);
+                browser.keys('Tab');
+                const hasValidClass = realNameField.getAttribute('class').includes('is-valid');
+                expect(hasValidClass).true;
+            });
+        });
+});
