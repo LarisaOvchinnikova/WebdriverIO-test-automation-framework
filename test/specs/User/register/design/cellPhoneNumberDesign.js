6ff5e6754c0d2fca70faf6d653eff9bbee9a49a4
Larisa <larisaplantation@gmail.com>
2019-09-27 17:35:35 -0400

    added tests to cellPhoneNumberDesign

--- test/specs/User/register/design/cellPhoneNumberDesign.js
+++ test/specs/User/register/design/cellPhoneNumberDesign.js
@@ -1,4 +1,4 @@
-
+//Tanya
 import { expect }  from 'chai';
 import { baseUrl } from './../../../../constants';
 
@@ -13,5 +13,18 @@ describe('Cell Phone Number input field', () => {
     it('should check that “Cell phone number” input field is displayed', () => {
         const actualInputFieldPhone = $(inputFieldPhone).isDisplayed();
         expect(actualInputFieldPhone).to.be.true;
+    });
+
+    it('should check border-color of "Cell phone number" input field ', () => {
+        const actualBorder = $(inputFieldPhone).getCSSProperty('border-top-color').parsed.hex;
+        const expectedBorder = '#ced4da';
+        expect(actualBorder).equal(expectedBorder);
+    });
+
+    it('should check font-color of "Cell phone number" input field when user enters first symbol', () => {
+        $(inputFieldPhone).setValue('+');
+        const actualFontColor = $(inputFieldPhone).getCSSProperty('color').parsed.hex;
+        const expectedFontColor = '#495057';
+        expect(actualFontColor).equal(expectedFontColor);
     });
 });
