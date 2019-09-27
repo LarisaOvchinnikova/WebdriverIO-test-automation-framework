02af30e4951582a51da7b91b16ef845ae21f5af3
Larisa <larisaplantation@gmail.com>
2019-09-27 17:29:01 -0400

    added test to cellPhoneNumberDesign

--- test/specs/User/register/design/cellPhoneNumberDesign.js
+++ test/specs/User/register/design/cellPhoneNumberDesign.js
@@ -7,7 +7,11 @@ const inputFieldPhone = '//input[@name="phone"]';
 
 describe('Cell Phone Number input field', () => {
     before(() => {
-        browser.url(url.registerUrl);
+        browser.url(url);
     });
 
+    it('should check that “Cell phone number” input field is displayed', () => {
+        const actualInputFieldPhone = $(inputFieldPhone).isDisplayed();
+        expect(actualInputFieldPhone).to.be.true;
+    });
 });
