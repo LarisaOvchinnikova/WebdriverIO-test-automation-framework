cce15406055a16ddcbddebeb076d41528890f8bc
Larisa <larisaplantation@gmail.com>
2019-10-02 17:01:36 -0400

    changed userConstants

--- test/specs/User/userConstants.js
+++ test/specs/User/userConstants.js
@@ -2,13 +2,23 @@
 
 //const loginUrl = 'https://stage.pasv.us/user/login';
 
-const admin = {
-   name: 'Lara Lara',
-   phone: '+19541111111',
-   email: 'larisa12345@gmail.com',
-   password: 'qwerty',
-   about: 'Hello, I am admin!',
-   goals: 'To find new job in QA.',
+const user = {
+   admin = {
+      name: 'Lara Lara',
+      phone: '+19541111111',
+      email: 'larisa12345@gmail.com',
+      password: 'qwerty',
+      about: 'Hello, I am admin!',
+      goals: 'To find new job in QA.',
+   },
+   student = {
+      name: 'Vasya Pupkin',
+      phone: '+19541111111',
+      email: 'vasya@gmail.com',
+      password: '12345',
+      about: 'I am student!',
+      goals: 'To study JS and QA.',
+   },
 };
 
-module.exports = { admin };
+module.exports = { user };
