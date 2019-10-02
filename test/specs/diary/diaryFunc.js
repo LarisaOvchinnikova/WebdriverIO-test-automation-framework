dc2621a2aecf2e7b0485afca053ee232a7010cfa
Larisa <larisaplantation@gmail.com>
2019-10-02 19:15:53 -0400

    added tests to diary - func

--- test/specs/diary/diaryFunc.js
+++ test/specs/diary/diaryFunc.js
@@ -6,8 +6,10 @@ import loginAction from '../../actions/loginActions';
 const menuDiarySelector = '//div[@id="site-menu"]//a[text() = "Diary"]';
 const headerSelector = '//h1';
 const diaryRecordSelector = '//div[@class="mt-2"]';
+const createDayReportButtonSelector = '//a[text()="Create day report"]';
 
 const diaryH1 = 'Day reports';
+const createDiaryH1 = 'Create day report';
 
 describe('Diary - Func', () => {
     before (() => {
@@ -15,26 +17,35 @@ describe('Diary - Func', () => {
     });
 
     it('should verify that `Diary` is displayed in main menu', () => {
-      const diaryIsDisplayed = $(menuDiarySelector).isDisplayed()
+      const diaryIsDisplayed = $(menuDiarySelector).isDisplayed();
       expect(diaryIsDisplayed).to.be.true;
     });
 
-    it('should verify that click to `Diary` in main menu should redirect to Diary page ', () => {
+    it('should verify that click to `Diary` in main menu should redirect to Diary page', () => {
         $(menuDiarySelector).click();
         const actualUrl = browser.getUrl();
         expect(actualUrl).to.equal(url.diaryUrl);
     });
 
-    it('should verify that Day reports page has correct h1', () => {
+    it('should verify that `Day reports` page has correct h1', () => {
         const actualh1Text = $(headerSelector).getText();
         expect(actualh1Text).to.equal(diaryH1);
     });
 
-    it('should verify that diary page has at least one record ', function () {
+    it('should verify that `Day reports` page has at least one record', function () {
         browser.pause(1000);
         const countOfRecords = $$(diaryRecordSelector).length;
         expect(countOfRecords > 0).to.be.true;
     });
 
+    it('should verify that click to `Create day report` button redirect to `Create day report` page', () => {
+        $(createDayReportButtonSelector).click();
+        const actualUrl = browser.getUrl();
+        expect(actualUrl).to.equal(url.createDayReportUrl);
+    });
 
+    it('should verify that `Create day report` page has correct h1', () => {
+        const actualh1Text = $(headerSelector).getText();
+        expect(actualh1Text).to.equal(createDiaryH1);
+    });
 });
