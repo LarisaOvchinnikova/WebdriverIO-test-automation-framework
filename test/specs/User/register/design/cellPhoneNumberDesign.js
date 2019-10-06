12e692f724fefba00390276ec9897d7dcc6a5979
Larisa <larisaplantation@gmail.com>
2019-10-06 19:48:14 -0400

    added test to register -cellPhoneNumber - design

--- test/specs/User/register/design/cellPhoneNumberDesign.js
+++ test/specs/User/register/design/cellPhoneNumberDesign.js
@@ -1,11 +1,12 @@
 import { expect }  from 'chai';
 import { url } from '../../../../actions/constants';
 
-const inputFieldPhone = '//input[@name="phone"]';
+const phoneField = '//input[@name="phone"]';
 
 const expectedBorderColor = '#ced4da';
 const expectedFontColor = '#495057';
-
+const expectedHighlightColor = '#4d94ff';
+const expectedBackgroundColor = '#ffffff';
 
 describe('Register - Cell Phone Number - Design', () => {
     before(() => {
@@ -13,19 +14,31 @@ describe('Register - Cell Phone Number - Design', () => {
     });
 
     it('should check that “Cell phone number” input field is displayed', () => {
-        expect($(inputFieldPhone).isDisplayed()).to.be.true;
+        expect($(phoneField).isDisplayed()).to.be.true;
     });
 
     it('should check border-color of "Cell phone number" input field ', () => {
-        const actualBorderColor = $(inputFieldPhone).getCSSProperty('border-top-color').parsed.hex;
+        const actualBorderColor = $(phoneField).getCSSProperty('border-top-color').parsed.hex;
         expect(actualBorderColor).equal(expectedBorderColor);
     });
 
     it('should check font-color of "Cell phone number" input field when user enters first symbol', () => {
-        $(inputFieldPhone).setValue('1');
-        const actualFontColor = $(inputFieldPhone).getCSSProperty('color').parsed.hex;
+        $(phoneField).setValue('1');
+        const actualFontColor = $(phoneField).getCSSProperty('color').parsed.hex;
         expect(actualFontColor).equal(expectedFontColor);
     });
+
+    it('should verify focus highlight color', function () {
+        const actualHighlightColor = $(phoneField).getCSSProperty('box-shadow').parsed.hex.toLowerCase();
+        expect(actualHighlightColor).equal(expectedHighlightColor);
+    });
+
+    it('should check background-color of "Cell phone number" input field when user enters first symbol', () => {
+        $(phoneField).setValue('1');
+        const actualBackgroundColor = $(phoneField).getCSSProperty('background-color').parsed.hex;
+        expect(actualBackgroundColor).equal(expectedBackgroundColor);
+    });
+
 
 
 });
