ae24b999c0ee38e964c1e675d83a419f452a824b
Larisa <larisaplantation@gmail.com>
2019-10-10 18:56:49 -0400

    change file diaryFunc

---
+++ test/specs/diary/_actions/diaryGetAll.js
@@ -0,0 +1,18 @@
+import { url } from '../../../actions/constants';
+import axios from 'axios';
+
+export default function diaryGetAll(token) {
+    return axios({
+        method: 'GET',
+        url: `${url.server}/diary`,
+        headers: {
+            Authorization: token,
+        },
+    })
+        .then(response => {
+            return response.data;
+        })
+        .catch(() => {
+            return null;
+        });
+}
