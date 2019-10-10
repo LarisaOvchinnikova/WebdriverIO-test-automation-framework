a0011b4a4c731e714f12392f3a3a07f9aa0334c4
Larisa <larisaplantation@gmail.com>
2019-10-09 21:24:17 -0400

    added tests to 'createGroup'

--- test/actions/constants.js
+++ test/actions/constants.js
@@ -3,6 +3,7 @@ export const url = {
     loginUrl: 'https://stage.pasv.us/user/login',
     registerUrl: 'https://stage.pasv.us/user/register',
     diaryUrl: 'https://stage.pasv.us/diary',
+    groupUrl: 'https://stage.pasv.us/group',
     createDayReportUrl: 'https://stage.pasv.us/diary/create',
     serverUrl: 'https://server-stage.pasv.us',
     userUrl: 'https://stage.pasv.us/user'
--- test/specs/groups/createGroup.js
+++ test/specs/groups/createGroup.js
@@ -1,13 +1,36 @@
 import { expect } from 'chai';
-import { user } from './../../actions/userConstants';
+//import { user } from './../../actions/userConstants';
 import { url } from './../../actions/constants';
 import loginAction from '../../actions/loginActions';
 
-describe('Diary - Func', () => {
+const selector = {
+    menuGroups: '//li/a[@qa="groups-link"]',
+    h1: '//h1',
+};
+const expected ={
+    h1: 'Groups',
+};
+
+describe('Groups - Create group - Functionality', () => {
     before(() => {
         browser.maximizeWindow();
         loginAction(browser);
     });
 
+    it('should verify that `Groups` item is displayed in main menu', () => {
+        const groupsIsDisplayed = $(selector.menuGroups).isDisplayed();
+        expect(groupsIsDisplayed).to.be.true;
+    });
+
+    it('should verify that click to `Groups` in main menu should redirect to `Groups` page', () => {
+        $(selector.menuGroups).click();
+        const actualUrl = browser.getUrl();
+        expect(actualUrl).to.equal(url.groupUrl);
+    });
+
+    it('should verify that `Groups` page has correct h1', () => {
+        const actualH1Text = $(selector.h1).getText();
+        expect(actualH1Text).to.equal(expected.h1);
+    });
 
 });
