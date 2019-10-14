21829ae8312a8c8c7cf56e8d9d675f08f857274b
Larisa <larisaplantation@gmail.com>
2019-10-14 10:59:57 -0400

    added test to "editGroup"

--- test/specs/groups/createLecture.js
+++ test/specs/groups/createLecture.js
@@ -54,24 +54,6 @@ describe('Groups - Edit group - Functionality', () => {
         loginAction(browser);
     });
 
-    it('should verify URL after group is created', () => {
-        $(selector.menuGroups).click();
-        $(selector.createGroupbutton).click();
-        $(selector.groupNameField).setValue(data.groupName);
-        $(selector.groupDescriptionField).setValue(data.groupDescription);
-        $(selector.accessTypeField).selectByVisibleText(data.accessType);
-        $(selector.submitButton).click();
-        browser.pause(1000);
-        const actualUrl = browser.getUrl();
-        expect(actualUrl).equal(url.group);
-    });
-
-    it('should verify that new group is displayed in the list of groups', () => {
-        browser.pause(1000);
-        const lastGroup = $$(selector.groupListItem)[0];
-        const lastGroupText = lastGroup.getText();
-        expect(lastGroupText.includes(data.groupName)).to.be.true;
-    });
 
     it('should verify that clicking on group name in the list of groups redirect to group`s page', () => {
         const lastGroupName = $$(selector.groupNames)[0];
@@ -80,64 +62,6 @@ describe('Groups - Edit group - Functionality', () => {
         const actualH1 = $(selector.h1).getText();
         const expectedH1 = `Group ${data.groupName}`;
         expect(actualH1).equal(expectedH1);
-    });
-
-    it('should verify that clicking on `Edit`button redirect to `Edit Group` page', () => {
-        $(selector.editButton).click();
-        browser.pause(1000);
-        const actualH1 = $(selector.h1).getText();
-        expect(actualH1).equal(expected.editGroupH1);
-    });
-
-    it('should verify that `Group name` field is displayed on `Edit Group` page', () => {
-        expect($(selector.groupNameField).isDisplayed()).to.be.true;
-    });
-
-    it('should verify that `Group description` field is displayed on `Edit Group` page', () => {
-        expect($(selector.editGroupDescription).isDisplayed()).to.be.true;
-    });
-
-    it('should verify that `Access type` field is displayed on `Edit Group` page', () => {
-        expect($(selector.accessTypeField).isDisplayed()).to.be.true;
-    });
-
-    it('should verify that `Slack Webhook slug` field is displayed on `Edit Group` page', () => {
-        expect($(selector.slackWebhook).isDisplayed()).to.be.true;
-    });
-
-    it('should verify that `Assign quiz` field is displayed on `Edit Group` page', () => {
-        expect($(selector.searchQuiz).isDisplayed()).to.be.true;
-    });
-
-    it('should verify that after editing and clicking on save button will redirect to `Groups` page', () => {
-        $(selector.groupNameField).setValue(data.newGroupName);
-        $(selector.editGroupDescription).setValue(data.newGroupDescription);
-        $(selector.accessTypeField).selectByVisibleText(data.newAccessType);
-        $(selector.submitButton).click();
-        browser.pause(1000);
-        const actualUrl = browser.getUrl();
-        expect(actualUrl).equal('https://stage.pasv.us/groups');
-    });
-
-    it('should verify that success message is displayed', () => {
-        expect($(selector.successMessage).isDisplayed()).to.be.true;
-    });
-
-    it('should verify that name of group was changed', () => {
-        $(selector.menuGroups).click();
-        browser.pause(6000);
-        const lastGroup = $$(selector.groupListItem)[0];
-        const lastGroupText = lastGroup.getText();
-        expect(lastGroupText.includes(data.newGroupName)).to.be.true;
-    });
-
-    it('should verify that Description on group page is correct', () => {
-        const lastGroupName = $$(selector.groupNames)[0];
-        lastGroupName.click();
-        browser.pause(1000);
-        $(selector.descriptionButton).click();
-        const actualDescriptionText = $(selector.descriptionText).getText();
-        expect(actualDescriptionText).include(data.newGroupDescription)
     });
 
     it('should verify that after click on `Lectures` item button `Create lecture` is displayed', () => {
