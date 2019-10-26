47de94d8bcdeb6ab1a8549c3ea15395eabc946e3
Larisa <larisaplantation@gmail.com>
2019-10-25 20:48:28 -0400

    added test for testing TJMAXX

--- test/sites_examples/tjmaxx.js
+++ test/sites_examples/tjmaxx.js
@@ -11,12 +11,13 @@ const expected = {
 
 };
 const data = {
-
+    search: 'linen dress Italy',
 };
 
 describe('TJMAXX - Search field -  functionality', () => {
     before (() => {
         browser.url(url);
+        browser.maximizeWindow();
     });
 
     it('should verify that logo is displayed', () => {
@@ -26,5 +27,12 @@ describe('TJMAXX - Search field -  functionality', () => {
     it('should verify that search field is displayed', () => {
         expect($(selector.searchField).isDisplayed()).true;
     });
+
+    it('should enter text in search field', () => {
+        $(selector.searchField).setValue(data.search);
+        browser.keys('Enter');
+        browser.pause(6000);
+    });
+
 
 });
