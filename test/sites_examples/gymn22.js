ebd481d0f2b9b6c9b15635c95955c4673c4703d9
Larisa <larisaplantation@gmail.com>
2019-11-06 07:28:41 -0500

    added test for gymn22 Minsk

--- test/sites_examples/gymn22.js
+++ test/sites_examples/gymn22.js
@@ -6,6 +6,7 @@ const selector = {
     minEdu: '//div[@id="wb_Text1"]',
     nameGymn: '//div[@id="wb_Image2"]//img[@id="Image1"]',
     searchField: '//input[@type="search"]',
+    loupe: '//input[@class="ya-site-form__submit"]',
 
 };
 
@@ -30,5 +31,9 @@ describe('Gumnazia 22 Minsk - Design', () => {
     it('should verify that search field is displayed', () => {
         const searchIsDisplayed  = $(selector.searchField).isDisplayed();
         expect(searchIsDisplayed).true;
+    });
+    it('should verify that loupe is displayed', () => {
+        const loupeIsDisplayed  = $(selector.loupe).isDisplayed();
+        expect(loupeIsDisplayed).true;
     });
 });
