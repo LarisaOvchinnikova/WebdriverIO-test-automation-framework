188356605ee6a4098a9b170e530c2447590aecca
Larisa <larisaplantation@gmail.com>
2019-11-19 21:43:28 -0500

    added test for walmart

--- test/sites_examples/walmart.js
+++ test/sites_examples/walmart.js
@@ -3,6 +3,7 @@ import { expect } from 'chai';
 const url = 'https://www.walmart.com/';
 
 const selector = {
+    logo: '//a[@id=\'vh-home-link\']//span[@class=\'g_a g_i\']',
     search: '//input[@id=\'global-search-input\']',
 
 };
@@ -19,7 +20,10 @@ describe('Facebook - functionality', () => {
         const searchIsDisplayed = $(selector.search).isDisplayed();
         expect(searchIsDisplayed).to.be.true;
     });
-
+    it('should verify logo is displayed', () => {
+        const logoIsDisplayed = $(selector.logo).isDisplayed();
+        expect(logoIsDisplayed).to.be.true;
+    });
 
 
 });
