cac926b1e3b84a42711fdf442738e2eb8f81ab94
Larisa <larisaplantation@gmail.com>
2019-11-19 21:58:02 -0500

    added test for walmart

--- test/sites_examples/walmart.js
+++ test/sites_examples/walmart.js
@@ -4,8 +4,9 @@ const url = 'https://www.walmart.com/';
 
 const selector = {
     logo: '//a[@id=\'vh-home-link\']//span[@class=\'g_a g_i\']',
-    search: '//input[@id=\'global-search-input\']',
+    search: '//input[@id="global-search-input"]',
     allDeparttmentsSearch: '//div[@id="search-category-bg"]',
+    lupa: '//button[@id=\'global-search-submit\']//span[@class=\'g_a g_f\']',
 
 };
 
@@ -21,13 +22,20 @@ describe('Facebook - functionality', () => {
         const searchIsDisplayed = $(selector.search).isDisplayed();
         expect(searchIsDisplayed).to.be.true;
     });
+
     it('should verify logo is displayed', () => {
         const logoIsDisplayed = $(selector.logo).isDisplayed();
         expect(logoIsDisplayed).to.be.true;
     });
+
     it('should verify All Departments drop menu is existing', () => {
         const allDepIsExisting = $(selector.allDeparttmentsSearch).isExisting();
         expect(allDepIsExisting).to.be.true;
     });
 
+    it('should enter text in search field', () => {
+        $(selector.search).setValue('vitamin C');
+        browser.pause(10000);
+
+    });
 });
