6e326987165a1d9989200aaa063e3002c58c2773
Larisa <larisaplantation@gmail.com>
2020-01-09 23:51:14 -0500

    added test for login stage.pasv.us

--- test/specs/User/loginForm/func/login.js
+++ test/specs/User/loginForm/func/login.js
@@ -4,6 +4,8 @@ const selector = {
     loginButton: '//a[@qa="login-link"]',
     h1: '//h1',
     emailField: '//input[@name="email"]',
+    passwordField: '//input[@name="password"]',
+    submitButton: '//button[@type="submit"]',
 };
 const expected = {
     h1: 'User Login',
@@ -30,5 +32,14 @@ describe('Login form', () => {
         expect ($(selector.emailField).isDisplayed()).to.be.true;
     });
 
+    it('should verify password field is displayed', () => {
+        expect ($(selector.passwordField).isDisplayed()).to.be.true;
+    });
 
+    it('should verify submit button is disabled', () => {
+      // expect($(selector.submitButton).getAttribute('class')).includes('disabled');
+        const submitButton = $(selector.submitButton);
+        const isEnabled = submitButton.isEnabled();
+        expect(isEnabled).equal(false);
+    });
 });
