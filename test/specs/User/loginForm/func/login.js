56936bea887ef189e37ee7c21adc3d338d096f34
Larisa <larisaplantation@gmail.com>
2020-01-09 23:28:09 -0500

    added test for login stage.pasv.us

--- test/specs/User/loginForm/func/login.js
+++ test/specs/User/loginForm/func/login.js
@@ -2,13 +2,16 @@ import {expect} from "chai";
 
 const selector = {
     loginButton: '//a[@qa="login-link"]',
-}
+};
 describe('Login form', () => {
     before (() => {
-      browser.url('https://stage.pasv.us/user/login');
+      browser.url('https://stage.pasv.us');
     });
 
     it('should click login button', () => {
       $(selector.loginButton).click();
+      const actualUrl = browser.getUrl();
+      const expectedUrl = 'https://stage.pasv.us/user/login';
+      expect (actualUrl).equal(expectedUrl)
     });
 });
