326aa0fadf73a684027138939f970d25e6cf8fea
Larisa <larisaplantation@gmail.com>
2020-01-11 10:44:27 -0500

    added unit test for sum

--- test/unitTests/index_spec.js
+++ test/unitTests/index_spec.js
@@ -1,8 +1,5 @@
 const assert = require('assert');
-function sum (a, b){
-    return a + b;
-}
-
+const sum = require('./../index');
 
 it('should sum equal 0', ()=>{
     assert.equal(sum(-5,5),0)
@@ -12,3 +9,6 @@ it('should sum equal 20', ()=>{
     assert.equal(sum(5,15),20)
 });
 
+it('should sum not equal 20', ()=>{
+    assert.notEqual(sum(1,15),20)
+});
