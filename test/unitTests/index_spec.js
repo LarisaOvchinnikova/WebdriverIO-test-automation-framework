fc0357064e8e7efc724315a90ee312dc719ea74b
Larisa <larisaplantation@gmail.com>
2020-01-11 10:40:12 -0500

    added unit test for sum

--- test/unitTests/index_spec.js
+++ test/unitTests/index_spec.js
@@ -3,6 +3,12 @@ function sum (a, b){
     return a + b;
 }
 
+
 it('should sum equal 0', ()=>{
     assert.equal(sum(-5,5),0)
 });
+
+it('should sum equal 20', ()=>{
+    assert.equal(sum(5,15),20)
+});
+
